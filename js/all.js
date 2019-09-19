b685838ea293f7085cd884d9276b56051b9758f3
Trung Nguyen <atom.911@hotmail.com>
2019-09-19 17:35:56 +0700

    cap nhat giao dien lan 5

--- js/all.js
+++ js/all.js
@@ -15,8 +15,30 @@ $(document).ready(function(){
         else{
             $('.grid').isotope({
                 filter: group
-              }); 
+                }); 
         }
-      
+    
     })
+    $('.fancybox').fancybox();
+    var counters = $(".count");
+    var countersQuantity = counters.length;
+    var counter = [];
+
+    for (i = 0; i < countersQuantity; i++) {
+        counter[i] = parseInt(counters[i].innerHTML);
+    }
+
+    var count = function(start, value, id) {
+        var localStart = start;
+        setInterval(function() {
+        if (localStart < value) {
+            localStart++;
+            counters[id].innerHTML = localStart;
+        }
+        }, 0.5);
+    }
+
+    for (j = 0; j < countersQuantity; j++) {
+        count(0, counter[j], j);
+    }
 })  
