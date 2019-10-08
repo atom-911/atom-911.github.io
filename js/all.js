6f5e026f09bce0ee9327308a2364c64ac49c07ce
Trung Nguyen <atom.911@hotmail.com>
2019-10-08 21:22:37 +0700

    update slider

--- js/all.js
+++ js/all.js
@@ -113,7 +113,7 @@ $(document).ready(function(){
             localStart++;
             counters[id].innerHTML = localStart;
         }
-        },2);
+        },5);
     }
 
     for (j = 0; j < countersQuantity; j++) {
@@ -145,4 +145,34 @@ $(document).ready(function(){
             
     });
     // https://script.google.com/macros/s/AKfycbyYiNm-OUSsBY8UGGwajJM9qQgZ13P44CSdaapLwT71q-or8rIh/exec
+
+    // Slick slider
+    
+    var sickPrimary = {
+    autoplay: true,
+    autoplaySpeed: 2400,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 1800,
+    cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+    asNavFor: '.text-slider',
+    centerMode: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+    }
+
+    var sickSecondary = {
+    autoplay: true,
+    autoplaySpeed: 2400,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1800,
+    cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+    asNavFor: '.image-slider',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+    }
+
+    $('.image-slider').slick(sickPrimary);
+    $('.text-slider').slick(sickSecondary);
 })  
