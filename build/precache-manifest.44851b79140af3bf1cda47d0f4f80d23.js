3b9be1af86c6937f22a6acebad6b66b442b8df13
Trung Nguyen <atom.911@hotmail.com>
2019-10-14 15:52:11 +0700

    update new website

---
+++ build/precache-manifest.44851b79140af3bf1cda47d0f4f80d23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09dba25ac63f8844bba9eaeafac4f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70be6adb2f4d37932a1",
+    "url": "/static/css/main.9f639a70.chunk.css"
+  },
+  {
+    "revision": "c0c97a09f1b47d6be009",
+    "url": "/static/js/2.cc0b5a87.chunk.js"
+  },
+  {
+    "revision": "a70be6adb2f4d37932a1",
+    "url": "/static/js/main.3e215d9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
